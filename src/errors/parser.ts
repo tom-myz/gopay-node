590a8ac12edc090f8b952eba61392e0639bc96e2
Karol Janyst <lapkom@gmail.com>
2016-11-17 10:42:43 +0900

    Fix missing http code

--- src/errors/parser.ts
+++ src/errors/parser.ts
@@ -33,7 +33,9 @@ export function fromError (error: Error): ErrorResponse {
             errors : [{[error.parameter]: "required"}]
         }
     } else if (error instanceof APIError) {
-        errorResponse = error.response ? error.response : { code : getCodeByStatus(error.status), httpCode : error.status }
+        errorResponse = Object.assign(
+            {}, error.response ? error.response : { code : getCodeByStatus(error.status) }, { httpCode : error.status }
+        )
     }
 
     return Object.assign({
