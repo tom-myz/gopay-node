2bbabff3162dfa07afe2791fb481514595d00100
Karol Janyst <lapkom@gmail.com>
2017-06-26 17:51:37 +0900

    Move security and rcurring token config to public sdk

--- src/resources/Merchants.ts
+++ src/resources/Merchants.ts
@@ -1,14 +1,26 @@
 import { ResponseCallback, AuthParams } from "../api/RestAPI"
 import { CRUDResource } from "./CRUDResource"
 import { ConfigurationItem } from "./common/Configuration"
+import { RecurringTokenPrivilege } from "./Verification"
 
 export type TransferPeriod = "weekly" | "biweekly" | "monthly"
 /* Request */
 
 /* Response */
+export interface RecurringTokenConfiguration {
+    recurringType?: RecurringTokenPrivilege
+    chargeWaitPeriod?: string
+}
+
+export interface SecurityConfiguration {
+    inspectSuspiciousLoginAfter?: string
+}
+
 export interface MerchantConfigurationItem extends ConfigurationItem {
     waitPeriod?: string
     transferPeriod?: TransferPeriod
+    recurringTokenConfiguration?: RecurringTokenConfiguration
+    securityConfiguration?: SecurityConfiguration
 }
 
 export interface MerchantItem {
