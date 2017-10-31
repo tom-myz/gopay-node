57e9f356ebf3d45ef3674cee0a1396d8d7b52eb3
Krzysztof Woliński <krzysztof.wolinski@xsolve.pl>
2017-10-31 13:58:25 +0900

    Updated TransferSchedule model (#102)

--- src/resources/common/TransferSchedule.ts
+++ src/resources/common/TransferSchedule.ts
@@ -1,12 +1,38 @@
 export const enum TransferPeriod {
-    WEEKLY   = "weekly",
-    BIWEEKLY = "biweekly",
-    MONTHLY  = "monthly"
+    WEEKLY          = "weekly",
+    BIWEEKLY        = "biweekly",
+    SEMI_MONTHLY    = "semi_monthly",
+    MONTHLY         = "monthly"
+}
+
+export const enum DayOfWeek {
+    SUNDAY      = "sunday",
+    MONDAY      = "monday",
+    TUESDAY     = "tuesday",
+    WEDNESDAY   = "wednesday",
+    THURSDAY    = "thursday",
+    FRIDAY      = "friday",
+    SATURDAY    = "saturday"
+}
+
+export const enum WeekOfMonth {
+    FIRST   = "first",
+    SECOND  = "second",
+    THIRD   = "third",
+    FOURTH  = "fourth",
+    LAST    = "last"
 }
 
 export interface TransferSchedule {
     period: TransferPeriod
-    dayOfWeek: number
-    weekOfMonth: number
+    dayOfWeek: DayOfWeek
+    weekOfMonth: WeekOfMonth
     dayOfMonth: number
+    weekly?: {
+        closingDay: DayOfWeek
+        payoutDay: DayOfWeek
+    }
+    monthly?: {
+        dayOfMonth: number
+    }
 }
